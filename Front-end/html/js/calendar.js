d40a898f16e521ee036a9f5556dbf7b13ca5c6a3
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-10-06 12:39:49 +0300

    fix: calendar

--- Front-end/html/js/calendar.js
+++ Front-end/html/js/calendar.js
@@ -573,7 +573,10 @@ class Calendar {
     }
 
     getEventsForDate(date) {
-        const dateStr = date.toISOString().split('T')[0];
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const dateStr = `${year}-${month}-${day}`;
         return this.events.filter(event => event.date === dateStr);
     }
 
